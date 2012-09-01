a1b2ba4ef799b594803630fec850e941e87bc7ff
Heily <Heily@192.168.10.102>
2012-09-01 19:23:51 +0300

    bootstrapi kasutamine

---
+++ index_vana.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" href="agentuur_style.css" type="text/css" />
+        <link rel="stylesheet" href="cssarrow.css" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="js/jQuery.js"></script>    
+        <title>Magna Labor</title>  
+    
+    </head> 
+    
+    <body>
+        <div id="wrap">
+            
+            <header>
+                <div id="logo"><h1>agentuur</h1></div>
+  
+                <div id ="lan">
+                    <ul>
+                        <li><a href="#">est</a></li>
+                        <li><a href="#">eng</a></li>
+                    </ul>            
+                 </div>
+            </header>
+            <div id="content">
+                <img src="http://placehold.it/200x200">
+            </div>
+            
+                <ul id="nav">
+                    <li><a href="#">meediavahendus</a>
+                        <ul>
+                            <li>trükimeedia</li>
+                            <li>välimeedia</li>
+                        </ul>
+                    </li>
+                    <li><a href="#">disain</a>
+                        <ul>
+                            <li>graafiline disain</li>
+                            <li>küljendus</li>
+                            <li>fototöötlus</li>
+                        </ul>
+                    </li>
+                    <li><a href="#">pr</a>
+                        <ul>
+                            <li>meediasuhted</li>
+                            <li>pressiteated</li>
+                            <li>pressiüritused</li>
+                        </ul>
+                    </li>
+                    <li><a href="#">tõlked</a>
+                        <ul>
+                            <li>kirjalikud tõlked</li>
+                            <li>suulised järeltõlked</li>
+                            <li>keele korrektuur</li>
+                        </ul>
+                    </li>
+              
+                    
+                    
+                    <li><a href="#">üritused</a>
+                        <ul>
+                            <li>vastuvõtud ja banketid</li>
+                            <li>firmapeod</li>
+                            <li>konverentsid</li>
+                            <li>pressiüritused</li>
+                            <li>koolitused</li>
+                        </ul>
+                    </li>
+                    <li><a href="#">tootmine</a>
+                        <ul>
+                            <li>lightboxid</li>
+                            <li>turvaväravate katted</li>
+                            <li>roll-upid ja lipud</li>
+                            <li>plakatid ja bännerid</li>
+                            <li>kleebised</li>
+                            <li>reklaamrõivad</li>
+                        </ul>
+                    </li>
+                          <li>
+                        <a href="#">trükised</a>
+                        <ul>
+                            <li>visiitkaardid<li>
+                            <li>kinkekaardid</li>
+                            <li>plakatid</li>
+                            <li>kataloogid ja kliendilehed</li>
+                            <li>flaierid ja voldikud</li>
+                        </ul>
+                    
+                    </li>
+                    
+                </ul>            
+             <span class="clearfix"></span>
+              <div class="arrow_box logo" style="margin: 3% 30% 0% 30%; padding: 1% 0% 1% 0%;">kontakt</div>
+             <footer>
+                
+                <h3 id="picture">pilt</h3>
+                <address>
+                            Lemme Leima<br />
+                            Projektijuht<br />
+                            5565432<br />
+                            meedia@magnalabor.ee<br /><br /> 
+
+                            Magna Labor OÜ<br />
+                            Madara 27<br />
+                            Tallinn 
+                            </address>
+                       
+                           <ul id="friends">
+                                <li>sõbrad</li>
+                                <li><a href="#">tempera</a></li>
+                                <li><a href="#">maasikalabor</a></li>
+                            </ul> 
+                            <span class="clearfix"></span>
+                       
+                
+            </footer>
+           <span class="clearfix"></span>
+               
+        </div> <!-- WRAP end -->
+    </body>
+</html>
